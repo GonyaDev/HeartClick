032bd687dd97eb0cc70cfb66cc86ec555a007a73
GonyaDev <gonyadev@yandex.ru>
2022-04-26 12:38:23 +0300

    Initial Commit

---
+++ HeartClick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeartClick
+//
+//  Created by Алексей Гончаров on 4/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeartClick/HeartClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartClickApp.swift
+//  HeartClick
+//
+//  Created by Алексей Гончаров on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct HeartClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
